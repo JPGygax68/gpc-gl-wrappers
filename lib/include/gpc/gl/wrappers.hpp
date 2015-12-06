e20c2c9f28b7258c52f9e11d4490fdfb79326e24
Jean-Pierre Gygax <gygax@practicomp.ch>
2015-12-06 22:01:39 +0100

    Improved version only needs a single macro for both void and non-void returning GL functions

--- lib/include/gpc/gl/wrappers.hpp
+++ lib/include/gpc/gl/wrappers.hpp
@@ -3,21 +3,74 @@
 #include <string>
 #include <stdexcept>
 #include <type_traits>
+#include <sstream>
 
 namespace gpc {
 
     namespace gl {
 
+        inline void check_previous_call(const char *text, int line, const char *file)
+        {
+            // Collect error codes (thanks to StackOverflow user Nicol Bolas for pointing out 
+            // that there could be more than one) into a string
+            bool flag = false;
+            std::stringstream error_codes;
+            for (GLenum error = GL_NO_ERROR; (error = glGetError()) != GL_NO_ERROR; )
+            {
+                if (flag) error_codes << ", "; else flag = true;
+                error_codes << error;
+            }
+
+            if (flag)
+            {
+                throw std::runtime_error(std::string(text) 
+                    + " failed at line " + std::to_string(line)
+                    + " in file " + file
+                    + " with OpenGL error code(s) " + error_codes.str()
+                    );
+            }
+        }
+
+        // Wrapper for GL calls returning non-void:
+        template <typename F, typename ...Args>
+        auto call(
+            std::enable_if_t<!std::is_void<std::result_of_t<F(Args...)>>::value, const char> *text,
+            int line,
+            const char *file,
+            F && f, Args &&... args
+            )
+        {
+            auto && res = std::forward<F>(f)(std::forward<Args>(args)...);
+            check_previous_call(text, line, file);
+            return res;
+        }
+
+        // Wrapper for GL calls that do not return a value:
+        template <typename F, typename ...Args>
+        void call(
+            std::enable_if_t<std::is_void<std::result_of_t<F(Args...)>>::value, const char> *text,
+            int line,
+            const char *file,
+            F && f, Args &&... args
+            )
+        {
+            std::forward<F>(f)(std::forward<Args>(args)...);
+            check_previous_call(text, line, file);
+        }
+
+        // The following functions are deprecated
+
         inline void _throw_error(const char *text, GLenum err, int line, const char *file)
         {
-            throw std::runtime_error(std::string(text) + 
-                ": error " + std::to_string((int)err) + 
-                " failed at line " + std::to_string(line) + 
+            throw std::runtime_error(std::string(text) +
+                ": error " + std::to_string((int)err) +
+                " failed at line " + std::to_string(line) +
                 " in file " + file
                 );
         }
 
         template<typename F, typename... Args>
+        [[deprecated]]
         auto func(const char *text, int line, const char *file, F fn, Args&&... args) -> decltype(fn(std::forward<Args>(args)...))
         {
             auto result = fn(std::forward<Args>(args)...);
@@ -28,61 +81,13 @@ namespace gpc {
         }
 
         template<typename P, typename... Args>
+        [[deprecated]]
         auto proc(const char *text, int line, const char *file, P pr, Args&&... args) -> decltype(pr(std::forward<Args>(args)...))
         {
             pr(std::forward<Args>(args)...);
             auto err = glGetError();
-            if (err != 0) _throw_error(text, err, line, file); 
+            if (err != 0) _throw_error(text, err, line, file);
         };
-
-        /*
-        template <typename F, typename ...Args>
-        struct returns_nonvoid : std::enable_if<!std::is_void<std::result_of_t<F(Args...)>>::value, std::result_of_t<F(Args...)>> {};
-
-        template <typename F, typename ...Args>
-        using nonvoid_return_t = typename returns_nonvoid<F, Args...>::type;
-
-        template <typename F, typename ...Args>
-        struct enable_if_returns_void : std::enable_if<std::is_void<std::result_of_t<F(Args...)>>::value, void> {};
-
-        template <typename F, typename ...Args>
-        using enable_if_returns_void_t = typename enable_if_returns_void<F, Args...>::type;
-        */
-
-        /*
-        template <typename F, typename ...Args>
-        using const_char_p_if_returns_void = std::enable_if_t < std::is_void<std::result_of_t<F(Args...)>>::value, const char *>;
-
-        template <typename F, typename ...Args>
-        using const_char_p_if_returns_nonvoid = std::enable_if_t < !std::is_void<std::result_of_t<F(Args...)>>::value, const char *>;
-        */
-
-        template <typename F, typename ...Args>
-        auto call(
-            std::enable_if_t<!std::is_void<std::result_of_t<F(Args...)>>::value, const char> *text,
-            int line,
-            const char *file,
-            F && f, Args &&... args
-            )
-        {
-            auto && res = std::forward<F>(f)(std::forward<Args>(args)...);
-            auto err = glGetError();
-            if (err != 0) _throw_error(text, err, line, file);
-            return res;
-        }
-
-        template <typename F, typename ...Args>
-        void call(
-            std::enable_if_t<std::is_void<std::result_of_t<F(Args...)>>::value, const char> *text,
-            int line,
-            const char *file,
-            F && f, Args &&... args
-            )
-        {
-            std::forward<F>(f)(std::forward<Args>(args)...);
-            auto err = glGetError();
-            if (err != 0) _throw_error(text, err, line, file);
-        }
 
     } // ns gl
     
@@ -97,14 +102,14 @@ namespace gpc {
 #define CALL_GL(fn, ...) ::gpc::gl::func(#fn, __LINE__, __FILE__, fn, __VA_ARGS__)
 #define EXEC_GL(pr, ...) ::gpc::gl::proc(#pr, __LINE__, __FILE__, pr, __VA_ARGS__)
 
-#define _GL(fn, ...) gpc::gl::call(#fn, __LINE__, __FILE__, gl##fn, __VA_ARGS__)
+#define GL(fn, ...) gpc::gl::call(#fn, __LINE__, __FILE__, gl##fn, __VA_ARGS__)
 
 #else
 
 #define CALL_GL(fn, ...) fn(__VA_ARGS__)
 #define EXEC_GL(pr, ...) pr(__VA_ARGS__)
 
-#define _GL(cl, ...) cl(__VA_ARGS__)
+#define GL(cl, ...) cl(__VA_ARGS__)
 
 #endif  
 
